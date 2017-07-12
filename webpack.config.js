c4ef56b6e0af454724a793a4a9d32faf3a9e0f29
eduardiliashenco <eduard.iliashenco@gmail.com>
2017-07-12 16:51:02 +0300

    sass, pug loaders congigured

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,80 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const fs = require('fs');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const extractSASS = new ExtractTextPlugin('[name].css');
+'use strict';
 
-module.exports = {
-  entry: './src/templates/',
+let webpack = require('webpack');
+let path = require('path');
+let autoprefixer = require('autoprefixer');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let extractStyles = new ExtractTextPlugin('[name].css')
+
+let config = {
+  stats: {
+    assets: false,
+    colors: true,
+    version: false,
+    hash: true,
+    timings: true,
+    chunks: false,
+    chunkModules: false
+  },
+  entry: {
+    index: [
+      path.resolve(__dirname, 'templates/index.pug')
+    ],
+    'css/application': [
+      path.resolve(__dirname, 'assets/styles/application.scss')
+    ]
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
   },
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: extractSASS.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-          }
-        }
-      },
-      {
         test: /\.pug$/,
-        use: [{
-          loader: 'pug-loader',
-          options: {
-            doctype: 'html'
-          }
-        }]
+        loader: ['html-loader', 'pug-html-loader?pretty&exports=false']
+      },
+      {
+        test: /\.scss$/,
+        loader: extractStyles.extract({
+          fallback: "style-loader",
+          loader: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ],
+  })
       }
     ]
   },
-  plugins: tmpls('./src/templates/')
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      options: {
+        postcss: [
+          autoprefixer({
+            browsers: ['last 2 version', 'Explorer >= 10', 'Android >= 4']
+          })
+        ],
+        sassLoader: {
+          includePaths: [
+            path.resolve(__dirname, 'node_modules/sanitize.css/')
+          ]
+        }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      title: 'poh',
+      template: 'templates/index.pug'
+    }),
+    extractStyles,
+  ]
 }
-;
 
-function tmpls(dir) {
-  let files = fs.readdirSync(dir);
-  let result = [];
-
-  files.forEach(function (itm) {
-    if (path.extname(itm) == '.pug')
-      result.push(
-        new HtmlWebpackPlugin({
-          filename: itm.replace(path.extname(itm), '.html'),
-          title: 'hiw',
-          template: './src/templates/' + itm
-        })
-      );
-  });
-
-  result.push(extractSASS);
-
-  return result;
-};
+module.exports = config;
